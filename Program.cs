9b3b4492d0681625835da24bcbbbcf122475c282
dasvinic <60906381+dasvinic@users.noreply.github.com>
2022-03-16 18:32:43 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace caio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string initial = "<<.<<..>><>><.>.>.<<.>.<.>>>><>><>>";
+            ///<<>><<><>>>><>><>>
+             ///>>>>
+            initial = initial.Replace(".", "");
+            int cont = 0;
+            string diamond = "<>";
+            Console.WriteLine("quantidade de diamantes : " + cont + " \t | Estado atual : " + initial);
+            while (initial.Contains(diamond))
+            {
+                
+                int x = initial.IndexOf(diamond, 0);
+                initial = initial.Remove(x, 2);
+                cont++;
+
+                Console.WriteLine("localizaçao do diamante : "+x +" \t | quantidade de diamantes : "+ cont +" \t | Estado atual : "+initial);
+            }
+
+
+
+
+           
+            Console.ReadKey();
+        }
+    }
+}
